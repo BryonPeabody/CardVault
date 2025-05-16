36af8fc08605875757ed3c93954036c174012caf
bryonpeabody <Bryon.Peabody@icloud.com>
2025-05-15 17:31:42 -0700

    Add CardForm

---
+++ vault/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Card
+
+
+class CardForm(forms.ModelForm):
+    class Meta:
+        model = Card
+        fields = ['name', 'set_code', 'language', 'card_number', 'condition']
