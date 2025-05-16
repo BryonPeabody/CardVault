79c064f99a14d9b3cb5ce70ba3f05f779d465ad4
bryonpeabody <Bryon.Peabody@icloud.com>
2025-05-15 18:26:52 -0700

    Add admin feature and link vault.urls in config.urls

--- vault/admin.py
+++ vault/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Card
 
-# Register your models here.
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'set_code', 'card_number', 'condition', 'language', 'value_usd')
+    list_filter = ('set_code', 'condition', 'language')
+    search_fields = ('name', 'set_code', 'card_number')
