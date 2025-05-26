2edd967f175c0e07eec4c0d2efabf2bf076979e9
bryonpeabody <Bryon.Peabody@icloud.com>
2025-05-26 12:19:10 -0700

    Change SET_CODE in models to dropdown menu dictionary reflecting json data for api call with intuitive UI

--- vault/models.py
+++ vault/models.py
@@ -20,9 +20,28 @@ class Card(models.Model):
         ("DE", "German"),
         # add more as needed
     ]
+    SET_CHOICES = [
+        ('sv01', 'Scarlet & Violet Base'),
+        ('sv02', 'Paldea Evolved'),
+        ('sv03', 'Obsidian Flames'),
+        ('sv03.5', '151'),
+        ('sv04', 'Paradox Rift'),
+        ('sv04.5', 'Paldean Fates'),
+        ('sv05', 'Temporal Forces'),
+        ('sv05.5', 'Twilight Masquerade'),
+        ('sv06', 'Shrouded Fable'),
+        ('sv07', 'Raging Surf'),
+        ('sv08', 'Surging Sparks'),
+        ('sv09', 'Prismatic Evolutions'),
+        ('sv10', 'Destined Rivals'),
+        ('svp', 'Scarlet & Violet Promos'),
+        ('swsh12', 'Silver Tempest'),
+        ('swsh12.5', 'Crown Zenith'),
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    set_code = models.CharField(max_length=10)
+    set_code = models.CharField(max_length=10, choices=SET_CHOICES)
     language = models.CharField(max_length=2, choices=LANGUAGE_CHOICES)
     card_number = models.CharField(max_length=10)
     condition = models.CharField(max_length=2, choices=CONDITION_CHOICES)
