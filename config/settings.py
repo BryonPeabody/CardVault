a50360df8eef58eee79c6a592a04954e3330cdad
bryonpeabody <Bryon.Peabody@icloud.com>
2025-06-19 16:09:34 -0700

    Hide SECRET_KEY and replace key

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z%wr*bjidf4u0!usw@odv$e8q3rew6-**x9++^avl_$0q8)*@c"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
