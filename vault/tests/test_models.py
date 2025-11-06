8f6f34be6b608d72517be8b6c956bb13cee96044
bryonpeabody <Bryon.Peabody@icloud.com>
2025-11-05 17:37:47 -0700

    Move user test fixture to conftest.py with related imports for global use

--- vault/tests/test_models.py
+++ vault/tests/test_models.py
@@ -2,15 +2,10 @@
 
 
 import pytest
-from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 from vault.models import Card
 
-
-@pytest.fixture
-def user(db):
-    return User.objects.create_user(username="testuser", password="password123")
 
 
 @pytest.mark.django_db
